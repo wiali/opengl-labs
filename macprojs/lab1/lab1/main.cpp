a91560a58f628b53d886a1bb469db44811f253b0
Roman <roman@MacBook-Pro-Roman.local>
2017-12-10 23:40:18 +0300

    run with working textures

--- macprojs/lab1/lab1/main.cpp
+++ macprojs/lab1/lab1/main.cpp
@@ -1,3 +1,169 @@
+/*
+ // working textures
+ #include <GLUT/glut.h>
+ #include <OpenGL/gl.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ #define stripeImageWidth 32
+ GLubyte stripeImage[4*stripeImageWidth];
+ 
+ static GLuint texName;
+ 
+ void makeStripeImage(void)
+ {
+ int j;
+ 
+ for (j = 0; j < stripeImageWidth; j++) {
+ stripeImage[4*j] = (GLubyte) ((j<=4) ? 255 : 0);
+ stripeImage[4*j+1] = (GLubyte) ((j>4) ? 255 : 0);
+ stripeImage[4*j+2] = (GLubyte) 0;
+ stripeImage[4*j+3] = (GLubyte) 255;
+ }
+ }
+ 
+ static GLfloat xequalzero[] = {1.0, 0.0, 0.0, 0.0};
+ static GLfloat slanted[] = {1.0, 1.0, 1.0, 0.0};
+ static GLfloat *currentCoeff;
+ static GLenum currentPlane;
+ static GLint currentGenMode;
+ 
+ void init(void)
+ {
+ glClearColor (0.0, 0.0, 0.0, 0.0);
+ glEnable(GL_DEPTH_TEST);
+ //    glShadeModel(GL_SMOOTH);
+ 
+ makeStripeImage();
+ glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+ 
+ glGenTextures(1, &texName);
+ glBindTexture(GL_TEXTURE_1D, texName);
+ glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+ glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER,
+ GL_LINEAR);
+ glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER,
+ GL_LINEAR);
+ glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, stripeImageWidth, 0,
+ GL_RGBA, GL_UNSIGNED_BYTE, stripeImage);
+ 
+ glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+ currentCoeff = xequalzero;
+ currentGenMode = GL_OBJECT_LINEAR;
+ currentPlane = GL_OBJECT_PLANE;
+ glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, currentGenMode);
+ glTexGenfv(GL_S, currentPlane, currentCoeff);
+ 
+ glEnable(GL_TEXTURE_GEN_S);
+ glEnable(GL_TEXTURE_1D);
+ glEnable(GL_CULL_FACE);
+ //    glEnable(GL_LIGHTING);
+ //    glEnable(GL_LIGHT0);
+ glEnable(GL_AUTO_NORMAL);
+ glEnable(GL_NORMALIZE);
+ glFrontFace(GL_CW);
+ glCullFace(GL_BACK);
+ glMaterialf (GL_FRONT, GL_SHININESS, 64.0);
+ }
+ 
+ void display(void)
+ {
+ glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ 
+ glPushMatrix ();
+ glRotatef(45.0, 0.0, 0.0, 1.0);
+ glBindTexture(GL_TEXTURE_1D, texName);
+ 
+ //    glutWireTorus(0.5, 0.5, 10, 10);
+ //    glutSolidTorus(2, 2, 4, 5);
+ glutSolidTeapot(2.0);
+ //    glutSolidOctahedron();
+ //    glutSolidSphere(2, 10, 10);
+ glutSolidTorus(0.1, 0.5, 10, 10);
+ glDisable(GL_TEXTURE_1D);
+ glPopMatrix ();
+ 
+ 
+ glutWireSphere(3, 10, 11);
+ 
+ glFlush();
+ glutSwapBuffers();
+ }
+ 
+ void reshape(int w, int h)
+ {
+ glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+ glMatrixMode(GL_PROJECTION);
+ glLoadIdentity();
+ if (w <= h)
+ glOrtho (-3.5, 3.5, -3.5*(GLfloat)h/(GLfloat)w,
+ 3.5*(GLfloat)h/(GLfloat)w, -3.5, 3.5);
+ else
+ glOrtho (-3.5*(GLfloat)w/(GLfloat)h,
+ 3.5*(GLfloat)w/(GLfloat)h, -3.5, 3.5, -3.5, 3.5);
+ glMatrixMode(GL_MODELVIEW);
+ glLoadIdentity();
+ }
+ 
+ void keyboard (unsigned char key, int x, int y)
+ {
+ switch (key) {
+ case 'e':
+ case 'E':
+ currentGenMode = GL_EYE_LINEAR;
+ currentPlane = GL_EYE_PLANE;
+ glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, currentGenMode);
+ glTexGenfv(GL_S, currentPlane, currentCoeff);
+ glutPostRedisplay();
+ break;
+ case 'o':
+ case 'O':
+ currentGenMode = GL_OBJECT_LINEAR;
+ currentPlane = GL_OBJECT_PLANE;
+ glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, currentGenMode);
+ glTexGenfv(GL_S, currentPlane, currentCoeff);
+ glutPostRedisplay();
+ break;
+ case 's':
+ case 'S':
+ currentCoeff = slanted;
+ glTexGenfv(GL_S, currentPlane, currentCoeff);
+ glutPostRedisplay();
+ break;
+ case 'x':
+ case 'X':
+ currentCoeff = xequalzero;
+ glTexGenfv(GL_S, currentPlane, currentCoeff);
+ glutPostRedisplay();
+ break;
+ case 27:
+ exit(0);
+ break;
+ default:
+ break;
+ }
+ }
+ 
+ int main(int argc, char** argv)
+ {
+ glutInit(&argc, argv);
+ glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+ glutInitWindowSize(800, 800);
+ //    glutInitWindowPosition(100, 100);
+ glutCreateWindow (argv[0]);
+ init();
+ 
+ //    GLuint ss = loadTexture("texture.bmp");
+ 
+ glutDisplayFunc(display);
+ glutReshapeFunc(reshape);
+ glutKeyboardFunc(keyboard);
+ glutMainLoop();
+ return 0;
+ }
+ */
+
+
 #include "task1.h"
 #include "task2.h"
 #include "task3.h"
@@ -33,12 +199,13 @@ void handleSpecialKeys();
 int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode (/*GLUT_DOUBLE | */GLUT_RGBA/* | GLUT_DEPTH*/);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     glutInitWindowSize(800, 800);
     glutCreateWindow("Lab1");
+    init();
     
     glutDisplayFunc(display);
-//    glutKeyboardFunc(keyboard);
+    //    glutKeyboardFunc(keyboard);
     glutSpecialFunc(specialKeys);
     
     glutMainLoop();
@@ -55,10 +222,10 @@ void display()
     glOrtho(-2.0, 2.0, -2.0, 2.0, 2.0, -2.0);
     
     handleSpecialKeys();
-
+    
     drawAxis();
     
-//    doTask123(positionX, positionY, positionZ);
+    //    doTask123(positionX, positionY, positionZ);
     doTask2();
     
     glFlush();
@@ -117,7 +284,6 @@ void drawAxis() {
     glVertex3f(0.f, 0.f, 1.f);
     glEnd();
 }
-
 
 //
 //// LOADING TEXTURE
@@ -385,168 +551,3 @@ void drawAxis() {
 //    glVertex3f(0.f, 0.f, 1.f);
 //    glEnd();
 //}
-
-
-/*
- #include <GLUT/glut.h>
- #include <OpenGL/gl.h>
- #include <stdlib.h>
- #include <stdio.h>
- 
- #define stripeImageWidth 32
- GLubyte stripeImage[4*stripeImageWidth];
- 
- static GLuint texName;
- 
- void makeStripeImage(void)
- {
- int j;
- 
- for (j = 0; j < stripeImageWidth; j++) {
- stripeImage[4*j] = (GLubyte) ((j<=4) ? 255 : 0);
- stripeImage[4*j+1] = (GLubyte) ((j>4) ? 255 : 0);
- stripeImage[4*j+2] = (GLubyte) 0;
- stripeImage[4*j+3] = (GLubyte) 255;
- }
- }
- 
- /*
- static GLfloat xequalzero[] = {1.0, 0.0, 0.0, 0.0};
- static GLfloat slanted[] = {1.0, 1.0, 1.0, 0.0};
- static GLfloat *currentCoeff;
- static GLenum currentPlane;
- static GLint currentGenMode;
- 
- void init(void)
- {
- glClearColor (0.0, 0.0, 0.0, 0.0);
- glEnable(GL_DEPTH_TEST);
- //    glShadeModel(GL_SMOOTH);
- 
- makeStripeImage();
- glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
- 
- glGenTextures(1, &texName);
- glBindTexture(GL_TEXTURE_1D, texName);
- glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
- glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER,
- GL_LINEAR);
- glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER,
- GL_LINEAR);
- glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, stripeImageWidth, 0,
- GL_RGBA, GL_UNSIGNED_BYTE, stripeImage);
- 
- glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
- currentCoeff = xequalzero;
- currentGenMode = GL_OBJECT_LINEAR;
- currentPlane = GL_OBJECT_PLANE;
- glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, currentGenMode);
- glTexGenfv(GL_S, currentPlane, currentCoeff);
- 
- glEnable(GL_TEXTURE_GEN_S);
- glEnable(GL_TEXTURE_1D);
- glEnable(GL_CULL_FACE);
- //    glEnable(GL_LIGHTING);
- //    glEnable(GL_LIGHT0);
- glEnable(GL_AUTO_NORMAL);
- glEnable(GL_NORMALIZE);
- glFrontFace(GL_CW);
- glCullFace(GL_BACK);
- glMaterialf (GL_FRONT, GL_SHININESS, 64.0);
- }
- 
- void display(void)
- {
- glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
- 
- glPushMatrix ();
- glRotatef(45.0, 0.0, 0.0, 1.0);
- glBindTexture(GL_TEXTURE_1D, texName);
- 
- //    glutWireTorus(0.5, 0.5, 10, 10);
- //    glutSolidTorus(2, 2, 4, 5);
- //    glutSolidTeapot(2.0);
- //    glutSolidOctahedron();
- //    glutSolidSphere(2, 10, 10);
- glutSolidTorus(0.1, 0.5, 10, 10);
- glDisable(GL_TEXTURE_2D);
- glPopMatrix ();
- glutSolidSphere(3, 10, 11);
- 
- glFlush();
- }
- 
- void reshape(int w, int h)
- {
- glViewport(0, 0, (GLsizei) w, (GLsizei) h);
- glMatrixMode(GL_PROJECTION);
- glLoadIdentity();
- if (w <= h)
- glOrtho (-3.5, 3.5, -3.5*(GLfloat)h/(GLfloat)w,
- 3.5*(GLfloat)h/(GLfloat)w, -3.5, 3.5);
- else
- glOrtho (-3.5*(GLfloat)w/(GLfloat)h,
- 3.5*(GLfloat)w/(GLfloat)h, -3.5, 3.5, -3.5, 3.5);
- glMatrixMode(GL_MODELVIEW);
- glLoadIdentity();
- }
- 
- void keyboard (unsigned char key, int x, int y)
- {
- switch (key) {
- case 'e':
- case 'E':
- currentGenMode = GL_EYE_LINEAR;
- currentPlane = GL_EYE_PLANE;
- glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, currentGenMode);
- glTexGenfv(GL_S, currentPlane, currentCoeff);
- glutPostRedisplay();
- break;
- case 'o':
- case 'O':
- currentGenMode = GL_OBJECT_LINEAR;
- currentPlane = GL_OBJECT_PLANE;
- glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, currentGenMode);
- glTexGenfv(GL_S, currentPlane, currentCoeff);
- glutPostRedisplay();
- break;
- case 's':
- case 'S':
- currentCoeff = slanted;
- glTexGenfv(GL_S, currentPlane, currentCoeff);
- glutPostRedisplay();
- break;
- case 'x':
- case 'X':
- currentCoeff = xequalzero;
- glTexGenfv(GL_S, currentPlane, currentCoeff);
- glutPostRedisplay();
- break;
- case 27:
- exit(0);
- break;
- default:
- break;
- }
- }
- 
- int main(int argc, char** argv)
- {
- glutInit(&argc, argv);
- glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
- glutInitWindowSize(800, 800);
- //    glutInitWindowPosition(100, 100);
- glutCreateWindow (argv[0]);
- //    init();
- 
- //    GLuint ss = loadTexture("texture.bmp");
- 
- glutDisplayFunc(display);
- glutReshapeFunc(reshape);
- glutKeyboardFunc(keyboard);
- glutMainLoop();
- return 0;
- }
- 
- 
- */
--- macprojs/lab1/lab1/task2.h
+++ macprojs/lab1/lab1/task2.h
@@ -18,6 +18,29 @@
 #endif
 #include <cmath>
 
+//from main start
+
+#define stripeImageWidth 32
+GLubyte stripeImage[4*stripeImageWidth];
+
+static GLuint texName;
+
+void makeStripeImage(void)
+{
+    int j;
+    
+    for (j = 0; j < stripeImageWidth; j++) {
+        stripeImage[4*j] = (GLubyte) ((j<=4) ? 255 : 0);
+        stripeImage[4*j+1] = (GLubyte) ((j>4) ? 255 : 0);
+        stripeImage[4*j+2] = (GLubyte) 0;
+        stripeImage[4*j+3] = (GLubyte) 255;
+    }
+}
+
+//from main finish
+
+
+
 
 void drawOctahedron();
 //void drawTorus(double r = 0.07, double c = 0.0, int rSeg = 16, int cSeg = 8);
@@ -27,13 +50,73 @@ void doTask2()
 {
     glColor3f(1.f, 1.f, 0.f);
     drawOctahedron();
-    drawTorus();
+//    drawTorus();
 }
 
 void drawOctahedron()
 {
-    glutWireOctahedron();
+//    glutWireOctahedron();
+    
+    
+    glPushMatrix ();
+    glRotatef(45.0, 0.0, 0.0, 1.0);
+    glBindTexture(GL_TEXTURE_1D, texName);
+    
+    //    glutWireTorus(0.5, 0.5, 10, 10);
+    //    glutSolidTorus(2, 2, 4, 5);
+        glutSolidTeapot(2.0);
+    //    glutSolidOctahedron();
+    //    glutSolidSphere(2, 10, 10);
+//    glutSolidTorus(0.1, 0.5, 10, 10);
+    glDisable(GL_TEXTURE_1D);
+    glPopMatrix ();
+    glutSolidSphere(3, 10, 11);
 }
+
+static GLfloat xequalzero[] = {1.0, 0.0, 0.0, 0.0};
+static GLfloat slanted[] = {1.0, 1.0, 1.0, 0.0};
+static GLfloat *currentCoeff;
+static GLenum currentPlane;
+static GLint currentGenMode;
+
+void init(void)
+{
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
+    //    glShadeModel(GL_SMOOTH);
+
+    makeStripeImage();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    glGenTextures(1, &texName);
+    glBindTexture(GL_TEXTURE_1D, texName);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER,
+                    GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR);
+    glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, stripeImageWidth, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, stripeImage);
+
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    currentCoeff = xequalzero;
+    currentGenMode = GL_OBJECT_LINEAR;
+    currentPlane = GL_OBJECT_PLANE;
+    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, currentGenMode);
+    glTexGenfv(GL_S, currentPlane, currentCoeff);
+
+    glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_1D);
+    glEnable(GL_CULL_FACE);
+    //    glEnable(GL_LIGHTING);
+    //    glEnable(GL_LIGHT0);
+    glEnable(GL_AUTO_NORMAL);
+    glEnable(GL_NORMALIZE);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
+    glMaterialf (GL_FRONT, GL_SHININESS, 64.0);
+}
+
 
 void drawTorus(double r, double c, int rSeg, int cSeg)
 {
